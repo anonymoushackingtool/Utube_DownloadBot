4fd1950da25f87a5755e123b0e92a2b25655cf1e
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-06-16 20:27:07 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,7 +5,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
 FORCE_SUB = os.environ.get("FORCE_SUB", "")
-WEBHOOK = os.environ.get("ANYTHING")
+WEBHOOK = os.environ.get("ANYTHING" ,"False")
 
 youtube_next_fetch = 0.30  # time in minute
 
