2b7ba33926c9b61b0eeb50bbc103d18a059c66b7
Anonymoushackingtool <74699731+anonymoushackingtool@users.noreply.github.com>
2021-06-16 19:49:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import  os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APP_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+FORCE_SUB = os.environ.get("FORCE_SUB", "")
 
 youtube_next_fetch = 0.30  # time in minute
 
